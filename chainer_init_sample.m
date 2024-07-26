93965c713f3d8de66e0d1f5f23203d8ec1ad8875
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2024-07-26 00:37:37 -0700

    cleanups

--- chainer_init_sample.m
+++ chainer_init_sample.m
@@ -1,18 +1,13 @@
 function sample = chainer_init_sample(params,opts)
-
-
 %% counter
 sample.i = 0;
 sample.F = get_F(sample.i,params);
 
-
 %% loads
 sample.b = rand(1,params.M) < 1/(1+exp(-params.b_prior_log_p1_m_log_p0));
 
-
 %% emission rates
 sample.h = params.h_prior_REF./params.h_prior_A.*randg(params.h_prior_A,params.N,1);
-
 
 %% atoms and dynamics
 sample.K = nan(       1,params.M);
@@ -22,10 +17,8 @@ sample.Z = nan(params.N,params.M);
 
 [sample.K,sample.X,sample.Y,sample.Z] = sampler_update_DDD(sample.K,sample.X,sample.Y,sample.Z,false(1,params.M),params);
 
-
 %% background
 sample.C = params.C_prior_REF./params.C_prior_A.*randg(params.C_prior_A,params.N,1);
-
 
 %% book-keeping
 sample.L = get_log_probs(sample.F,sample.C,sample.b,sample.h,sample.K,sample.X,sample.Y,sample.Z,params);
@@ -34,3 +27,4 @@ sample.D = get_D(sample.X,sample.Y,sample.Z,sample.b,params);
 sample.rec = repmat([0;realmin;0],1,9);
 
 
+end
